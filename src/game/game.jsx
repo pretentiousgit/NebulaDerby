b9a784f6202cd2ec4b62304161d14e655a8ed637
Alex Leitch <alex.leitch@gmail.com>
2017-08-31 12:21:59 -0400

    got game settings working

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import io from "socket.io-client";
 
 const socket = io();
 
-class Game extends Component {
-  render() {
-    return (
-      <div>
-        "Hello world"
-      </div>
-    )
-  }
+const Game = ({ }) => {
+  return (
+    <div>
+      "Hello world"
+    </div>
+  )
 }
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './App';
+import Game from './game/game';
+
 import registerServiceWorker from './registerServiceWorker';
 
 import store from './redux/store';
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={Game} />
+      <Route path='/admin' component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
